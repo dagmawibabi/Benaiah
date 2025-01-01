fde35b30f54cb841f319d41c6666c2e340b0a743
dagmawibabi <1babidagi@gmail.com>
2025-01-01 19:59:57 +0300

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import whiteLogo from '$lib/assets/logo/WhiteLogo.png';
 </script>
 
-<div class="flex h-screen w-screen flex-col items-center justify-center bg-black">
+<div class="flex h-screen w-screen flex-col items-center justify-center bg-black pb-48">
 	<img src={whiteLogo} alt="logo" class="object-fit w-56" />
 	<div class="pt-5 font-mono text-white">God has Created | Built</div>
 </div>
