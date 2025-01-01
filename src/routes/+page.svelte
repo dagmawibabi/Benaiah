6686e24a09667d6538302429087fc16c3baf5c60
dagmawibabi <1babidagi@gmail.com>
2025-01-01 19:49:02 +0300

    domain

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import blackLogo from '$lib/assets/logo/BlackLogo.png';
+	import whiteLogo from '$lib/assets/logo/WhiteLogo.png';
+</script>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center bg-black">
+	<img src={whiteLogo} alt="logo" class="object-fit w-56" />
+	<div class="pt-5 font-mono text-white">God has Created | Built</div>
+</div>
