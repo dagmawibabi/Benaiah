ac93b4727b41db2cbf06ddd09705e54d0b9dbd3b
Naol Fekadu <nf.naol9@gmail.com>
2025-06-23 10:08:38 +0300

    Audio support

--- src/routes/articles/themes/[theme]/[subtopic]/[slug]/+page.svelte
+++ src/routes/articles/themes/[theme]/[subtopic]/[slug]/+page.svelte
@@ -16,6 +16,8 @@
 <!-- Blurred Background -->
 <BlurredBackground reading_mode={true} />
 
+
+
 <div class="no-scrollbar relative h-screen overflow-scroll pb-2">
 	<!-- Content -->
 	<div class="w-[90%] md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2 m-auto items-center text-base md:text-lg lg:text-lg xl:text-lg 2xl:text-lg justify-center pt-8 text-left text-black">
